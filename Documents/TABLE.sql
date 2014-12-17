3e4e8d79decef4592d5f3ee372de872678f4a46f
JPomichael <jpomichael@hotmail.com>
2014-12-17 16:58:42 +0800

    ADD  Resource

---
+++ Documents/TABLE.sql
@@ -0,0 +1,12 @@
+--1. RoomRateWS
+select * from RoomRateWS 
+--delete from RoomRateWS
+
+--2. RateCodeControl
+select * from RateCodeControl
+--delete from RateCodeControl
+
+--3. RateCodeInfor
+select * from RateCodeInfor
+--delete from RateCodeInfor
+
