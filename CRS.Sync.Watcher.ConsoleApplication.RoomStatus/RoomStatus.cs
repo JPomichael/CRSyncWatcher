084b6bcbf8c39c5307503a60ad5cbfa60acf0201
JPomichael <jpomichael@hotmail.com>
2015-01-20 14:45:38 +0800

    add roomStatus Program

---
+++ CRS.Sync.Watcher.ConsoleApplication.RoomStatus/RoomStatus.cs
@@ -0,0 +1,39 @@
+﻿using CRS.Sync.Watcher.DLL;
+using CRS.Sync.Watcher.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CRS.Sync.Watcher.ConsoleApplication.RoomStatus
+{
+    public class RoomStatus
+    {
+        CRS.Sync.Watcher.Service.RatePlan.IRatePlanService _ratePlanService = new CRS.Sync.Watcher.Service.RatePlan.RatePlanService();
+        CRS.Sync.Watcher.Service.Hotel.IHotelService _hotelService = new CRS.Sync.Watcher.Service.Hotel.HotelService();
+
+
+
+
+
+
+        /// <summary>
+        /// 获取公寓信息-动态
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<hotel_info> GetCRSHotelList()
+        {
+            Expression<Func<hotel_info, bool>> expression = PredicateBuilder.True<hotel_info>();
+            expression = expression.And(o => o.source_id == null ? false : o.source_id == 6);
+            IEnumerable<hotel_info> _result = _hotelService.GetHoteListInfo(expression).AsEnumerable();
+            if (_result != null)
+            {
+                return _result;
+            }
+            else
+                return null;
+        }
+
+    }
+}
